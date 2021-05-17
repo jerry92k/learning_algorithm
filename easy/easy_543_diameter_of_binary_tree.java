4d1acb598ec1eb2d8697c93ec0224f09e9a3b25e
jerry92k <62507373+jerry92k@users.noreply.github.com>
2021-05-17 18:00:32 +0900

    easy 543 refactoring

--- easy/easy_543_diameter_of_binary_tree.java
+++ easy/easy_543_diameter_of_binary_tree.java
@@ -8,35 +8,43 @@
  * 각 루트에서 양쪽의 길이 차를 계산하고 차가 가장 큰 경우를 longest length로 한다.
  */
 class Solution {
+
+    private int maxLen = 0;
+
     public int diameterOfBinaryTree(TreeNode root) {
 
-        int[] maxLen=new int[1];
-
-        int sumLen = 2+traversal(root.left, maxLen) + traversal(root.right, maxLen);
-        if (maxLen[0] < sumLen) {
+        int sumLen = 2 + traversal(root.left) + traversal(root.right);
+        if (getMaxLen() < sumLen) {
             return sumLen;
         }
-        return maxLen[0];
+        return getMaxLen();
 
     }
 
-    public int traversal(TreeNode root, int[] maxLen){
+    public int getMaxLen() {
+        return this.maxLen;
+    }
+
+    public void setMaxLen(int maxLen) {
+        this.maxLen = maxLen;
+    }
+
+    public int traversal(TreeNode root) {
 
         if (root == null) {
             return -1;
         }
-        int leftLen=1+traversal(root.left, maxLen);
-        int rightLen=1+traversal(root.right, maxLen);
+        int leftLen = 1 + traversal(root.left);
+        int rightLen = 1 + traversal(root.right);
 
-        int sumLen=leftLen + rightLen;
-        if (maxLen[0] < leftLen + rightLen) {
-            maxLen[0] = sumLen;
+        int sumLen = leftLen + rightLen;
+        if (getMaxLen() < leftLen + rightLen) {
+            setMaxLen(sumLen);
         }
 
         if (leftLen >= rightLen) {
             return leftLen;
-        }
-        else{
+        } else {
             return rightLen;
         }
     }
