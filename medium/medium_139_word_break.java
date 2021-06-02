5fec018d222ae3866fbb476c99498b9a5d0559c2
jerry92k <62507373+jerry92k@users.noreply.github.com>
2021-06-02 10:45:21 +0900

    medium_139

--- medium/medium_139_word_break.java
+++ medium/medium_139_word_break.java
@@ -21,7 +21,7 @@ import java.util.*;
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int wordLen = s.length();
-        HashSet<String> wordDictionary = new HashSet<String>();
+        Set<String> wordDictionary = new HashSet<String>();
         Boolean[][] wordDictSubset = new Boolean[wordLen][wordLen]; // false로 초기화
 
         for (String word : wordDict) {
@@ -29,24 +29,24 @@ class Solution {
         }
 
         for (int i = 0; i < wordLen; i++) {
-            // for (int j = 0; j < i; j++) {
-            // wordDictSubset[i][j] = false;
-            // }
+            boolean isPromise = false;
+            if (i == 0 || wordDictSubset[0][i - 1]) {
+                isPromise = true;
+            }
 
             for (int j = i; j < wordLen; j++) {
-                if (wordDictionary.contains(s.substring(i, j + 1))) {
-                    wordDictSubset[i][j] = true;
-
-                    if (i > 0 && wordDictSubset[0][i - 1]) {
+                if (isPromise) {
+                    if (wordDictionary.contains(s.substring(i, j + 1))) {
+                        if (j == wordLen - 1) {
+                            return true;
+                        }
                         wordDictSubset[0][j] = true;
+
+                    } else {
+                        wordDictSubset[i][j] = false;
                     }
-                } else {
-                    wordDictSubset[i][j] = false;
                 }
             }
-        }
-        if (wordDictSubset[0][wordLen - 1]) {
-            return true;
         }
         return false;
     }
