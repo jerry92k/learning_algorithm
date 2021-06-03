d08e2dd4e9e111bc871bfc2c1992be9874808d2e
jerry92k <62507373+jerry92k@users.noreply.github.com>
2021-06-03 17:47:37 +0900

    medium 198, 740

--- medium/medium_740_delete_and_earn.java
+++ medium/medium_740_delete_and_earn.java
@@ -7,20 +7,20 @@ class Solution {
         int using = 0;
         int prev = -1;
 
-        for (int k = 0; k <= 10000; ++k)
+        for (int k = 0; k <= 10000; ++k) {
             if (count[k] > 0) {
                 int m = Math.max(avoid, using);
-                
                 // k번째를 포함하는 경우
                 if (k - 1 != prev) { // 인접하지 않은 경우, 이전까지 최대 합 + k번째값
-                    using = k * count[k] + m;  
+                    using = k * count[k] + m;
                 } else { // 인접한 경우, 이전까지 최대값 - 이전 인접값
                     using = k * count[k] + avoid; // 이번값 + 인접원소를 더하지 않은 최대값
                 }
                 // k번째를 포함하지 않는 경우 => avoid = k를 제외한 이전까지의 최대합
-                avoid = m; 
+                avoid = m;
                 prev = k;
             }
+        }
         return Math.max(avoid, using);
     }
 }
