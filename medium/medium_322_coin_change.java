819dbd8a9ca1c09b8ea86793e1edeed31ecb8064
jerry92k <62507373+jerry92k@users.noreply.github.com>
2021-06-03 17:47:16 +0900

    medium_740,198,322

---
+++ medium/medium_322_coin_change.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        int[] countForVal = new int[amount + 1];
+
+        Arrays.sort(coins);
+
+        for (int i = 1; i < countForVal.length; i++) {
+            calculateCount(i, countForVal, coins);
+        }
+        return countForVal[amount];
+    }
+    /*
+     * [1,2,5] 11
+     */
+
+    public void calculateCount(int subAmount, int[] countForVal, int[] coins) {
+
+        int coinCount = -1;
+        for (int i = 0; i < coins.length; i++) {
+            int remainVal = subAmount - coins[i];
+            if (remainVal < 0) {
+                continue;
+            } else if (remainVal == 0) {
+                countForVal[subAmount] = 1;
+                return;
+            } else {
+                if (countForVal[remainVal] > 0 && (coinCount == -1 || coinCount > countForVal[remainVal])) {
+                    coinCount = countForVal[remainVal];
+                }
+            }
+        }
+        countForVal[subAmount] = coinCount == -1 ? -1 : coinCount + 1;
+    }
+}
