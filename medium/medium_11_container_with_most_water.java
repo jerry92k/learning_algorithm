e0ac7d4633cf6a0e3e4d84c333fd3ca4d100f59e
jerry92k <jens92.k@gmail.com>
2021-06-23 21:57:12 +0900

    medium 11

---
+++ medium/medium_11_container_with_most_water.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public int maxArea(int[] height) {
+
+        // [2,3,10,5,7,8,9]
+        //  0 1 2  3 4 5 6
+        // 24
+        int inputLength = height.length;
+        
+        int maxProduct = 0;
+
+        int i = 0;
+        int j = inputLength - 1;
+        while (i<j) {
+
+           int productBetweenTwo = Math.min(height[i], height[j]) * (j - i);
+           maxProduct = Math.max(maxProduct, productBetweenTwo);
+           // 짧은 길이의 점을 그대로 두고 긴 길이의 점을 옮겨 더 긴 길이를 만나더라도 h의 기준은 짧은 길이이고 너비만 줄어들 뿐이므로
+           // h가 커질수있는 후보를 찾기위해 짧은 길이의 점을 움직임
+           if(height[i]<height[j]){
+               i++;
+           }else{
+               j--;
+           }
+            
+        }
+        return maxProduct;
+    }
+
+    // brute force
+    public int maxArea2(int[] height) {
+
+        int inputLength=height.length;
+      //  int[][] dp = new int[inputLength][inputLength];
+        int maxProduct=0;
+
+        for (int i = 0; i < inputLength; i++) {
+
+            for (int j = i + 1; j < inputLength; j++) {
+                int h = height[i] > height[j] ? height[j] : height[i];
+                int subProduct = h * (j - i);
+                if (subProduct > maxProduct) {
+                    maxProduct = subProduct;
+                }
+            }
+        }
+        return maxProduct;
+    }
+}
