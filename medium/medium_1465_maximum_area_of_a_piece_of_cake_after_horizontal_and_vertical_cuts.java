a0bc6e0710c1b6ed312743560b4a8f1230e2759e
jerry92k <62507373+jerry92k@users.noreply.github.com>
2021-06-25 18:01:34 +0900

    medium 1465

---
+++ medium/medium_1465_maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts.java
@@ -0,0 +1,40 @@
+/*
+You are given a rectangular cake of size h x w and two arrays of integers horizontalCuts and verticalCuts where:
+
+horizontalCuts[i] is the distance from the top of the rectangular cake to the ith horizontal cut and similarly, and
+verticalCuts[j] is the distance from the left of the rectangular cake to the jth vertical cut.
+Return the maximum area of a piece of cake after you cut at each horizontal and vertical position provided in the arrays horizontalCuts and verticalCuts. Since the answer can be a large number, return this modulo 109 + 7.
+
+ Input: h = 5, w = 4, horizontalCuts = [1,2,4], verticalCuts = [1,3]
+Output: 4 
+Explanation: The figure above represents the given rectangular cake. Red lines are the horizontal and vertical cuts. After you cut the cake, the green piece of cake has the maximum area.
+
+5
+4
+[3]
+[3]
+*/
+
+import java.util.*;
+
+class Solution {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+
+        int maxHeight = h - horizontalCuts[horizontalCuts.length - 1];
+        maxHeight = Math.max(maxHeight, horizontalCuts[0]);
+        for (int i = 0; i < horizontalCuts.length - 1; i++) {
+            int diff = horizontalCuts[i + 1] - horizontalCuts[i];
+            maxHeight = Math.max(maxHeight, diff);
+        }
+
+        int maxWidth = w - verticalCuts[verticalCuts.length - 1];
+        maxWidth = Math.max(maxWidth, verticalCuts[0]);
+        for (int i = 0; i < verticalCuts.length - 1; i++) {
+            int diff = verticalCuts[i + 1] - verticalCuts[i];
+            maxWidth = Math.max(maxWidth, diff);
+        }
+        return maxHeight * maxWidth;
+    }
+}
